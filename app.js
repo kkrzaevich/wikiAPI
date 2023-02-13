834918a0b789112e66fa1257cfa7ebc9d046d395
Konstantin Korolev <konstantinrzaevich@gmail.com>
2023-02-13 08:48:56 +0300

    Yay this is working version

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const ejs = require("ejs");
 app.set('view engine', 'ejs');
+const _ = require('lodash');
 
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
@@ -17,25 +18,26 @@ const Article = mongoose.model('Article', postSchema);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.post(`/articles`, (req, res) => {
-    const newArticle = new BlogPost({ title: req.body.title, content: req.body.content });
+// Requests targeting all articles
+
+app.route('/articles')
+  .post((req, res) => {
+    const newArticle = new Article({ title: req.body.title, content: req.body.content });
     newArticle.save();
     res.redirect('/articles');
 })
-
-app.get(`/articles`, (req, res) => {
+  .get((req, res) => {
     if (req.originalUrl.includes('favicon.ico')) {
       res.status(204).end()
     } else {
       Article.find({}, function(err,items) {
-        if (err) { res.send(err) }
+        if (err) { res.send('No such articles found :(') }
         else {
             res.send(items);
         }   
     });  }
 })
-
-app.delete(`/articles`, (req, res) => {
+  .delete((req, res) => {
     if (req.originalUrl.includes('favicon.ico')) {
       res.status(204).end()
     } else {
@@ -44,6 +46,54 @@ app.delete(`/articles`, (req, res) => {
       });   
     }
 })
+
+// Requests targeting a specific article
+
+app.route('/articles/:articleName')
+.get((req, res) => {
+  if (req.originalUrl.includes('favicon.ico')) {
+    res.status(204).end()
+  } else {
+    Article.findOne({title: req.params.articleName}, function(err,item) {
+      if (err) { res.send('No such articles found :(') }
+      else {
+          res.send(item);
+      }   
+  });  }
+})
+.put((req, res) => {
+  if (req.originalUrl.includes('favicon.ico')) {
+    res.status(204).end()
+  } else {
+    // Без overwrite: true будет вести себя как patch
+    Article.findOneAndUpdate({title: req.params.articleName}, { title: req.body.title, content: req.body.content }, {overwrite: true}, function(err,item) {
+      if (err) { res.send('Could not update an article :(') }
+      else {
+          res.send(item);
+      }   
+  });  }
+})
+.patch((req, res) => {
+  if (req.originalUrl.includes('favicon.ico')) {
+    res.status(204).end()
+  } else {
+    Article.findOneAndUpdate({title: req.params.articleName}, { title: req.body.title, content: req.body.content }, function(err,item) {
+      if (err) { res.send('Could not update an article :(') }
+      else {
+          res.send(item);
+      }   
+  });  }
+})
+.delete((req, res) => {
+  if (req.originalUrl.includes('favicon.ico')) {
+    res.status(204).end()
+  } else {
+    Article.deleteOne({title: req.params.articleName}, function(err) {
+      res.send(`Article ${req.params.articleName} deleted.`)
+    });   
+  }
+})
+
 
 app.listen(3000, function(req) {
     console.log("Server started on port 3000");
